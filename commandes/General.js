99a0afc867320ef5e5316337861688fdcc175669
Neyraa Tech ♥️ <leonardtrch001@gmail.com>
2025-09-01 15:36:56 +0300

    Add files via upload
    Leonard md

--- commandes/General.js
+++ commandes/General.js
@@ -95,7 +95,7 @@ zokou({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOpti
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
   repondre("look on pm sir ")
-  await zk.sendMessage(auteurMessage,{text :`https://whatsapp.com/channel/0029VakLfckBlHpYVxryFJ14`},{quoted :ms})
+  await zk.sendMessage(auteurMessage,{text :`https://whatsapp.com/channel/0029VbBAqgd4inooNwRQ4K0J`},{quoted :ms})
 
 })
 
