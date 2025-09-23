7b31799e2d90487bbfdf8ce1d63c02b3be3e9ee5
Neyraa Tech ♥️ <leonardtrch001@gmail.com>
2025-09-23 16:19:54 +0300

    Add files via upload
    Leonard md

--- commandes/neyraa.js
+++ commandes/neyraa.js
@@ -1,14 +1,45 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "ney", reaction: "💋", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Hello my name is  *Neyraa Nei* \n\n ' + "i'm a wife of *LeonardTech 😊💞 ";
-    let d = ' by *my owner*';
-    let varmess = z + d;
-    var img = 'https://files.catbox.moe/04qe9z.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
+const { downloadMediaMessage, downloadContentFromMessage } = require("@whiskeysockets/baileys");
+const { exec } = require('child_process');
+const { writeFile } = require("fs/promises");
+const fs = require('fs-extra');
+const moment = require("moment-timezone");
+
+
+zokou({
+  nomCom: 'report',
+  aliases: 'spread',
+  desc: 'report anything to the bot developer',
+  categorie: "new",
+  reaction: '🍂'
+}, async (bot, zk, context) => {
+  const { arg, repondre, superUser, nomAuteurMessage } = context;
+
+  if (!arg[0]) {
+    return repondre("After the command *broadcast*, type your message to be sent to the specified contacts.");
+  }
+
+  if (!superUser) {
+    return repondre("Only for the owner.");
+  }
+
+  // Specified contacts
+  const contacts = [
+    '255757103671@s.whatsapp.net',
+    '255757103671@s.whatsapp.net',
+    '255757103671@s.whatsapp.net'
+  ];
+
+  await repondre("*Leonard Md ☢️☢️-BOT is sending your message to Developer contacts 🤦🤷*...");
+
+  const broadcastMessage = `*𝗥𝗲𝗽𝗼𝗿𝘁 𝗠𝗲𝘀𝘀𝗮𝗴𝗲*\n
+𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${arg.join(" ")}\n
+𝗦𝗲𝗻𝗱𝗲𝗿 𝗡𝗮𝗺𝗲 : ${nomAuteurMessage}`;
+
+  for (let contact of contacts) {
+    await zk.sendMessage(contact, {
+      image: { url: 'https://files.catbox.moe/bpfjv2.jpg' },
+      caption: broadcastMessage
+    });
+  }
 });
-console.log("mon ney");
-/* one love
